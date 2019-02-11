cb9bd7c93fe71e12108cc177406dfa719a7daa45
Elder Moraes <elder.moraes@gmail.com>
2019-02-11 14:33:28 -0200

    Fixed debit function

--- debit/src/main/java/com/eldermoraes/fn/DebitFunction.java
+++ debit/src/main/java/com/eldermoraes/fn/DebitFunction.java
@@ -15,39 +15,31 @@
  */
 package com.eldermoraes.fn;
 
-import com.eldermoraes.fn.messages.AmountReq;
-import com.eldermoraes.fn.messages.TransactionRes;
 import com.fnproject.fn.api.FnConfiguration;
-import com.fnproject.fn.api.FnFeature;
 import com.fnproject.fn.api.RuntimeContext;
-import java.io.Serializable;
-import com.fnproject.fn.api.flow.Flow;
-import com.fnproject.fn.api.flow.FlowFuture;
-import com.fnproject.fn.api.flow.Flows;
-import com.fnproject.fn.runtime.flow.FlowFeature;
 
-@FnFeature(FlowFeature.class)
-public class DebitFunction implements Serializable{
+public class DebitFunction {
 
-    String funcDebit;
-    String funcCredit;
     
     @FnConfiguration
     public void configure(RuntimeContext ctx) {
-        funcDebit = ctx.getConfigurationByKey("DEBIT-ID")
-                .orElseThrow(() -> new RuntimeException("Missing FunctionId"));
-        
-        funcCredit = ctx.getConfigurationByKey("CREDIT-ID")
-                .orElseThrow(() -> new RuntimeException("Missing FunctionId"));
+        String accountApiURL = ctx.getConfigurationByKey("ACCOUNT_API_URL")
+                .orElseThrow(() -> new RuntimeException("No URL endpoint was provided."));
+        //TODO setup account API 
     }
     
-    public void handleRequest(AmountReq input) {
-        Flow f = Flows.currentFlow();
-        
-        FlowFuture<TransactionRes> debitFuture =
-                f.invokeFunction(funcDebit, input.debit, TransactionRes.class);
-        
-        
+    public static class DebitRequest{
+        public Double value;
+    }
+    
+    public static class DebitResponse{
+        public String confirmation;
+    }
+    
+    public DebitResponse debit(DebitRequest input){
+        DebitResponse resp = new DebitResponse();
+        resp.confirmation = "Account debited"; //TODO create an API
+        return resp;
     }
 
 }
